04182a05d927b76eaaaa2a53dbaa2310fb0f7979
Oleg'Ease'Kharchuk <olegease@gmail.com>
2024-09-28 02:38:59 +0300

    not time

--- include/son8/c/time.hxx
+++ include/son8/c/time.hxx
@@ -5,19 +5,5 @@
 #include "libc.hxx"
 
 namespace c {
-    // time
-    using std::clock_t;
-    using std::clock;
-    using std::time_t;
-    using std::ctime;
-    using std::difftime;
-    using std::time;
-    using std::timespec;
-    using std::timespec_get;
-    using std::tm;
-    using std::asctime;
-    using std::strftime;
-    using std::gmtime;
-    using std::localtime;
-    using std::mktime;
+
 }
